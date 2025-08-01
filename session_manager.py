b23e11cf1a9c4fd81ee292c34226bb257b3cfc7b
Saisha Goel <saishagoel005@gmail.com>
2025-08-02 01:29:27 +0530

    updated with session keys

--- session_manager.py
+++ session_manager.py
@@ -1,28 +1,31 @@
 import streamlit as st
+from session_keys import (
+    CURRENT_STAGE, PROCESSING_RESULTS, FLASHCARDS, STUDY_SETTINGS,
+    CURRENT_CARD, SHOW_ANSWER, STUDY_STATS
+)
 
 def init_session_state():
     """Initialize session state variables"""
-    if 'current_stage' not in st.session_state:
-        st.session_state.current_stage = 1
-    if 'processing_results' not in st.session_state:
-        st.session_state.processing_results = {}
-    if 'flashcards' not in st.session_state:
-        st.session_state.flashcards = []
-    if 'study_settings' not in st.session_state:
-        st.session_state.study_settings = {
-            'num_flashcards': 10,
+    if CURRENT_STAGE not in st.session_state:
+        st.session_state[CURRENT_STAGE] = 1
+    if PROCESSING_RESULTS not in st.session_state:
+        st.session_state[PROCESSING_RESULTS] = {}
+    if FLASHCARDS not in st.session_state:
+        st.session_state[FLASHCARDS] = []
+    if STUDY_SETTINGS not in st.session_state:
+        st.session_state[STUDY_SETTINGS] = {
+            'num_flashcards': 20,
             'difficulty': 'Mixed (Recommended)'
         }
     # Initialize flashcard study variables
-    if 'current_card' not in st.session_state:
-        st.session_state.current_card = 0
-    if 'show_answer' not in st.session_state:
-        st.session_state.show_answer = False
-    if 'study_stats' not in st.session_state:
-        st.session_state.study_stats = {'correct': 0, 'incorrect': 0, 'total': 0}
+    if CURRENT_CARD not in st.session_state:
+        st.session_state[CURRENT_CARD] = 0
+    if SHOW_ANSWER not in st.session_state:
+        st.session_state[SHOW_ANSWER] = False
+    if STUDY_STATS not in st.session_state:
+        st.session_state[STUDY_STATS] = {'correct': 0, 'incorrect': 0, 'total': 0}
 
 def reset_session():
     """Reset session state"""
     for key in list(st.session_state.keys()):
         del st.session_state[key]
-    st.rerun()
